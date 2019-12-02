f41a2277f7fed209b3656cde0f0d4b4f55b35ed4
Bennet Yee <bennet.yee@gmail.com>
2019-12-01 17:23:04 -0800

    simulate hand shuffling

--- shuffle.js
+++ shuffle.js
@@ -29,6 +29,9 @@ function positionStatistics(position, shuffler, deckSize, numSamples) {
     var counts = Array(deckSize).fill(0);
     for (var i = 0; i < numSamples; i++) {
 	var d = shuffler(deckSize);
+//	if (!isValidShuffle(d)) {
+//	    console.log(d);
+//	}
 	assert(isValidShuffle(d));
 	counts[d[position]]++;
     }
@@ -77,9 +80,80 @@ function shuffleNaive(deckSize) {
     return deck;
 }
 
+function shuffleCrazyOnce(inDeck, slipRange, canStartR) {
+    var split = Math.floor(inDeck.length / 2);
+    var lslice = inDeck.slice(0, split);
+    var rslice = inDeck.slice(split, inDeck.length);
+    var lpos = 0;
+    var rpos = 0;
+    var outDeck = Array();
+
+    var skipLeft = false;
+    if (canStartR) {
+	skipLeft = Math.random() < 0.5;
+    }
+    while (lpos < lslice.length || rpos < rslice.length) {
+	var avail = lslice.length - lpos;
+	if (avail > 0 && !skipLeft) {
+	    var numCards = Math.floor(Math.random() * slipRange) + 1;
+	    if (numCards > avail) {
+		numCards = avail;
+	    }
+	    for (var i = 0; i < numCards; i++) {
+		outDeck.push(lslice[lpos + i]);
+	    }
+	    lpos += numCards;
+	}
+	skipLeft = false;
+	avail = rslice.length - rpos;
+	if (avail > 0) {
+	    var numCards = Math.floor(Math.random() * slipRange) + 1;
+	    if (numCards > avail) {
+		numCards = avail;
+	    }
+	    for (var i = 0; i < numCards; i++) {
+		outDeck.push(rslice[rpos + i]);
+	    }
+	    rpos += numCards;
+	}
+    }
+    return outDeck;
+}
+
+function shuffleCrazy(deckSize, minCards, slipRange, count) {
+    var deck = initialDeck(deckSize);
+    for (var i = 0; i < count; i++) {
+	deck = shuffleCrazyOnce(deck, minCards, slipRange);
+    }
+    return deck;
+}
+
 function evaluateShuffles(deckSize, numSamples) {
-    var shufflers = new Map([["standard", shuffleStandard],
-			     ["naïve", shuffleNaive]]);
+    var shufflers = new Map([
+	["standard", shuffleStandard],
+	["naïve", shuffleNaive],
+	["perfect1", function (deckSize) {
+	    return shuffleCrazy(deckSize, 1, false, 1);
+	}],
+	["perfect7", function (deckSize) {
+	    return shuffleCrazy(deckSize, 1, false, 7);
+	}],
+	["semiperfect1", function (deckSize) {
+	    return shuffleCrazy(deckSize, 1, true, 1);
+	}],
+	["semiperfect7", function (deckSize) {
+	    return shuffleCrazy(deckSize, 1, true, 7);
+	}],
+	["slip3count4", function (deckSize) {
+	    return shuffleCrazy(deckSize, 3, true, 4);
+	}],
+	["slip3count7", function (deckSize) {
+	    return shuffleCrazy(deckSize, 3, true, 7);
+	}],
+	["slip5count7", function (deckSize) {
+	    return shuffleCrazy(deckSize, 5, true, 7);
+	}],
+    ]);
     var results = Array();
     for (var name of shufflers.keys()) {
 	results.push([name, lastPositionVariance(shufflers.get(name),
@@ -89,4 +163,4 @@ function evaluateShuffles(deckSize, numSamples) {
     return results;
 }
 
-console.log(evaluateShuffles(4, 100000));
+console.log(evaluateShuffles(52, 100000));
